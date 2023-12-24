ea1e2878908848284613d70fad4b3d9e97f0ab8b
shammaskhann <shammas.csgo0312@gmail.com>
2023-12-24 20:53:55 +0500

    commit

--- myScript.js
+++ myScript.js
@@ -14,23 +14,23 @@ $(document).ready(function() {
         parallax_height();
     });
 });
-// $(document).ready(function() {
-//     function parallax_height() {
-//         var scroll_top = $(this).scrollTop();
-//         var sample_section_top = $(".sample-section").offset().top;
-//         var header_height = $("header").outerHeight(); // Use the actual header height
-//         $(".sample-section").css({ "margin-top": header_height });
+    // $(document).ready(function() {
+    //     function parallax_height() {
+    //         var scroll_top = $(this).scrollTop();
+    //         var sample_section_top = $(".sample-section").offset().top;
+    //         var header_height = $("header").outerHeight(); // Use the actual header height
+    //         $(".sample-section").css({ "margin-top": header_height });
 
-//         // Only adjust the header height if the page is not being scrolled
-//         if (scroll_top === 0) {
-//             $(".sample-header").css({ height: header_height });
-//         }
-//     }
-//     parallax_height();
-//     $(window).scroll(function() {
-//         parallax_height();
-//     });
-//     $(window).resize(function() {
-//         parallax_height();
-//     });
-// });
+    //         // Only adjust the header height if the page is not being scrolled
+    //         if (scroll_top === 0) {
+    //             $(".sample-header").css({ height: header_height });
+    //         }
+    //     }
+    //     parallax_height();
+    //     $(window).scroll(function() {
+    //         parallax_height();
+    //     });
+    //     $(window).resize(function() {
+    //         parallax_height();
+    //     });
+    // });
